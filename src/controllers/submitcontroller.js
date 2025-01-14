dd930e0755e862dc51f9e838a156f616523ca210
Raghav Gupta 2 <raghavgupta1@Raghavs-MacBook-Pro-2.local>
2025-01-14 15:07:35 +0530

    Initiliased

--- src/controllers/submitcontroller.js
+++ src/controllers/submitcontroller.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Submits = require('../models/submitModel');
-
+const Events =  require('../models/eventsModel');
 const postSubmit = async (req, res) => {
     try {
         const { eventId, studentDetails } = req.body;
@@ -17,12 +17,23 @@ const postSubmit = async (req, res) => {
 
         const savedSubmit = await newSubmit.save();
 
+        const event = await Events.findById(eventId);
+        if (!event) {
+            return res.status(404).json({
+                success: false,
+                message: "Event not found.",
+            });
+        }
+        // event.registrationcount = (event.registrationcount || 0) + 1;
+        await Events.findByIdAndUpdate(eventId,{Registered:event.Registered+1})
+
         return res.status(201).json({
             success: true,
             message: "Submission created successfully.",
         });
     } catch (error) {
         console.error("Error creating submission:", error);
+
 
         return res.status(500).json({
             success: false,
