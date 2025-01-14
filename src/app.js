ce059f928b29cad10b207fc65768c40bffe803c3
Raghav Gupta 2 <raghavgupta1@Raghavs-MacBook-Pro-2.local>
2025-01-15 00:44:32 +0530

    Initiliased

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const db = require('../config/db')
-
+const cors = require("cors")
 app.use(express.json());
+app.use(cors())
 
 
 app.get('/', (req, res) => {
