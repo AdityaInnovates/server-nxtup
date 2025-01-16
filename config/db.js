f8abb3c91a294821563cc180c9096488c7f9783a
Aditya kumar <adityakr80510@gmail.com>
2025-01-17 04:03:51 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-async function connectDB(){
-    try{
-        await mongoose.connect("mongodb+srv://udit:udituditudit@cluster0.mmib2.mongodb.net/MERADB?retryWrites=true&w=majority&appName=Cluster0")
-        console.log('Successfully connected to the database');
-    }
-    catch(error){
-        console.error('Error connecting to the database:', error);
-    }
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.KEY);
+    console.log("Successfully connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
 }
-
 
 connectDB();
